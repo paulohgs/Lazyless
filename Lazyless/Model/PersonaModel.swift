034359d2f8d950bb04ea83bedbf46b4bc432e541
Luiz Sena <senaluiz1307@gmail.com>
2022-09-12 10:58:35 -0300

    ajustes no PersonaModel

---
+++ Lazyless/Model/PersonaModel.swift
@@ -0,0 +1,15 @@
+//
+//  PersonaModel.swift
+//  Lazyless
+//
+//  Created by Luiz Sena on 12/09/22.
+//
+
+import Foundation
+
+struct Persona {
+    var personaName: String
+    var personaImageName: String
+}
+
+var preguicaModel = Persona(personaName: "Preguica-Oneswan", personaImageName: "preguica")
