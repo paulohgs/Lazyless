ef4bd52ab64b07b799acef2d2385105d581882ce
Luiz Sena <senaluiz1307@gmail.com>
2022-09-14 10:56:35 -0300

    Refatorado e modelado

--- Lazyless/ViewController/MainController.swift
+++ Lazyless/ViewController/MainController.swift
@@ -11,60 +11,69 @@ import Lottie
 class MainController: UIViewController {
     
     private let backgroundView: MainBackgroundView = MainBackgroundView()
-    private var imageTeste: UIImageView = UIImageView(image: UIImage(named: preguicaModel.personaImageName))
+    private var personaImage: UIImageView = UIImageView(image: UIImage(named: preguicaModel.personaImageName))
+    private var imageHeart: UIImageView = UIImageView(image: UIImage(named: preguicaModel.heartImageName))
+    //    private var circularProgressBar: CircularProgressView = CircularProgressView(frame: CGRect(x: 0, y: 0, width: 250, height: 250), lineWidth: 5, rounded: true)
 
-    private var circularProgressBar: CircularProgressView = CircularProgressView(frame: CGRect(x: 0, y: 0, width: 250, height: 250), lineWidth: 5, rounded: true)
-
-    private var imageHeart: UIImageView = UIImageView(image: UIImage(named: preguicaModel.heart))
-    
     override func loadView() {
-        
         super.loadView()
-        imageTeste.translatesAutoresizingMaskIntoConstraints = false
-        imageHeart.translatesAutoresizingMaskIntoConstraints = false
-//        circularProgressBar.translatesAutoresizingMaskIntoConstraints = false
+        //        circularProgressBar.translatesAutoresizingMaskIntoConstraints = false
         
-        let tapGR = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
-        imageTeste.addGestureRecognizer(tapGR)
-        imageTeste.isUserInteractionEnabled = true
-        
-        self.view = backgroundView
-        self.view.addSubview(imageTeste)
-        self.view.addSubview(imageHeart)
-        
-// MARK: Barra de progresso circular config
-//        circularProgressBar.progressColor = .blue
-//        circularProgressBar.trackColor = .lightGray
-//        circularProgressBar.progress = 0.98
-// MARK: -----------------------------------------------
-        imageHeart.contentMode = .scaleAspectFit
-        imageTeste.contentMode = .scaleAspectFit
-        
-        constraints()
-    }
-   
-    func constraints(){
-        NSLayoutConstraint.activate([
-            imageTeste.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            imageTeste.topAnchor.constraint(equalToSystemSpacingBelow: self.view.topAnchor, multiplier: 3),
-            imageTeste.widthAnchor.constraint(equalTo: view.widthAnchor ,multiplier: 0.75),
-            imageTeste.heightAnchor.constraint(equalTo: self.imageTeste.widthAnchor, multiplier: 0.85),
+        let tapGR = UITapGestureRecognizer(
+            target: self,
+            action: #selector(imageTapped)
+        )
+        personaImage.addGestureRecognizer(tapGR)
+        personaImage.isUserInteractionEnabled = true
 
-            
+        // MARK: -Barra de progresso circular config
+        //        circularProgressBar.progressColor = .blue
+        //        circularProgressBar.trackColor = .lightGray
+        //        circularProgressBar.progress = 0.98
+        // MARK: -----------------------------------------------
+        
+        buildLayout()
+    }
+}
+
+extension MainController: ViewCoding {
+
+
+    func setupHierarchy() {
+        
+        self.view.addSubview(personaImage)
+        self.view.addSubview(imageHeart)
+    }
+
+    func setupContrainsts() {
+        personaImage.translatesAutoresizingMaskIntoConstraints = false
+        imageHeart.translatesAutoresizingMaskIntoConstraints = false
+
+        imageHeart.contentMode = .scaleAspectFit
+        personaImage.contentMode = .scaleAspectFit
+
+        NSLayoutConstraint.activate([
+            personaImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            personaImage.topAnchor.constraint(equalToSystemSpacingBelow: self.view.topAnchor, multiplier: 3),
+            personaImage.widthAnchor.constraint(equalTo: view.widthAnchor ,multiplier: 0.75),
+            personaImage.heightAnchor.constraint(equalTo: self.personaImage.widthAnchor, multiplier: 0.85),
+
+
             imageHeart.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            imageHeart.centerYAnchor.constraint(equalTo: self.imageTeste.bottomAnchor),
+            imageHeart.centerYAnchor.constraint(equalTo: self.personaImage.bottomAnchor),
             imageHeart.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.15),
             imageHeart.heightAnchor.constraint(equalTo: self.imageHeart.widthAnchor),
 
         ])
-        
     }
-    
+    func setupView() {
+        self.view = backgroundView
+    }
+    //  MARK: -objc funcs
     @objc func imageTapped(sender: UITapGestureRecognizer) {
         if sender.state == .ended {
             print("UIImageView tapped")
         }
     }
-    
-}
 
+}
